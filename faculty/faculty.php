9243e726e460bbb2711adf6c78cd4f4680caac87
Amna Pervez <amnapervez8910@gmail.com>
2026-09-03 15:18:49 +0500

    Update faculty.php

--- faculty/faculty.php
+++ faculty/faculty.php
@@ -21,7 +21,7 @@ $conn->close();
 
 <head>
   <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=2.0">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Faculty Dashboard</title>
 
   <style>
